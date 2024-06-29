375b0cb202d7e4098bcd5ae2e1c2bdbac299e940
Adarsh Pandey <adarshpandeyy00@gmail.com>
2024-06-29 15:42:04 +0530

    check1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("------------ABC BANK------------------");
+        System.Console.WriteLine("Login Page :");
+
+        string username = null;
+        string password = null;
+
+        System.Console.WriteLine("Enter Username");
+        username = System.Console.ReadLine();
+
+        if (username != "")
+        {
+            System.Console.WriteLine("Enter Password");
+            password = System.Console.ReadLine();
+        }
+
+        if(username=="system" && password=="manager")
+        {
+            System.Console.WriteLine();
+        }
+
+        System.Console.WriteLine("Thank you");
+        System.Console.ReadKey();
+        
+    }
+}
